fd4c8ffe2087f368201de44b675caa8ad46b7182
action <action@github.com>
2024-09-20 10:08:32 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Sep 20 2024 10:01:03 GMT+0000 (Coordinated Universal Time)
+//Fri Sep 20 2024 10:08:32 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("夸克网盘"),
